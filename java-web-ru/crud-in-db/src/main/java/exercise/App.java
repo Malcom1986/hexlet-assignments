f33fda495096ad789ee9f3c71d2dff732c976b98
@hexlet/cli <support@hexlet.io>
2023-06-21 10:36:42 +0300

    submit java-web-ru/crud-in-db

--- java-web-ru/crud-in-db/src/main/java/exercise/App.java
+++ java-web-ru/crud-in-db/src/main/java/exercise/App.java
@@ -32,7 +32,7 @@ public class App {
         if (port != null) {
             return Integer.valueOf(port);
         }
-        return 5000;
+        return 8000;
     }
 
     private static String getFileContent(String fileName) throws IOException {
